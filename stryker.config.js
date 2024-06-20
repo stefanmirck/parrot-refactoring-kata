4da609313472f63a497f402bccf5baf1218f50a0
Stefan Mirck <stefan@undefined.build>
2024-06-20 15:14:16 +0200

    build: add stryker to enable mutation testing

---
+++ stryker.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+	packageManager: 'npm',
+	reporters: ['html', 'clear-text', 'progress'],
+	coverageAnalysis: 'off',
+};
+
+export default config;
